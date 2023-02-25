e4c291f5533235e194fc7e0a5c8c9e1bc9129116
Prince Kumar <112944356+sinhaprince35@users.noreply.github.com>
2023-02-25 11:17:44 +0530

    Add files via upload

---
+++ React-Children-Props---Working-with-APIs-React-React-Router-React-Authenticat---nhshmi0itpag/src/components/App.js
@@ -0,0 +1,25 @@
+import React , {useState,useEffect} from 'react'
+import '../styles/App.css';
+import ChildComponent from './ChildComponent';
+const App = () => {
+ //code here
+ const [input1, SetInp1] = useState(0)
+
+  return (
+    <div id="main">
+      <ChildComponent input={
+       <input id='input' onChange={(e)=>{
+   let ans = e.target.value;
+   ans = Math.pow(ans, 2);
+   SetInp1(ans)
+   
+  }} />}
+ /> 
+    
+      <p id='output'>{input1}</p>
+    </div>
+  )
+}
+
+
+export default App;
---
+++ React-Children-Props---Working-with-APIs-React-React-Router-React-Authenticat---nhshmi0itpag/src/components/ChildComponent.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ChildComponent = ({input}) => {
+//code here
+  return (
+    <div id="child">
+      Find The Square <br/>
+    {input}
+     
+    </div>
+  )
+}
+
+
+export default ChildComponent;
