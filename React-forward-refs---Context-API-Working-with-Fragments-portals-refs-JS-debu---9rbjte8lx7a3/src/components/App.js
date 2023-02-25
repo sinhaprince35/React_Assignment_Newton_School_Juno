577c249c1bbf284ba03225fe14b2d6f7b28417e7
Prince Kumar <112944356+sinhaprince35@users.noreply.github.com>
2023-02-25 11:19:47 +0530

    Add files via upload

---
+++ React-forward-refs---Context-API-Working-with-Fragments-portals-refs-JS-debu---9rbjte8lx7a3/src/components/App.js
@@ -0,0 +1,28 @@
+import React, { useRef, useState, useEffect } from 'react'
+import '../styles/App.css';
+import InputField from './InputField.js';
+
+const App = () => {
+  const newRef = useRef();
+  const [values, setValues] = useState("");
+
+  const focusInput = () => {
+    newRef.current.focus();
+  };
+
+  const settingValue = () => {
+    setValues(newRef.current.value);
+  }
+
+  return (
+    <div>
+      <InputField ref={newRef} type="text" /><br />
+      <button id="settingValueButton" onClick={settingValue}>Set Value</button>
+      <button id="focusInputButton" onClick={focusInput}>Focus the input</button><br /><br />
+      <textarea id="textarea" defaultValue={values}></textarea>
+    </div>
+  );
+}
+
+
+export default App;
---
+++ React-forward-refs---Context-API-Working-with-Fragments-portals-refs-JS-debu---9rbjte8lx7a3/src/components/InputField.js
@@ -0,0 +1,13 @@
+import React, { useRef, useState, useEffect, forwardRef } from 'react';
+import '../styles/App.css';
+
+//complete the following using forward refs concept  
+
+const InputField = forwardRef((props, ref) => {
+  return (
+    <div>
+      <input id="input" type="text" ref={ref} />
+    </div>
+  )
+})
+export default InputField;
