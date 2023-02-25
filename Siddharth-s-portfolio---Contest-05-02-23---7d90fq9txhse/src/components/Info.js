4ffeeed1d5c8cd6f0dde46fa41f0138688456d8a
Prince Kumar <112944356+sinhaprince35@users.noreply.github.com>
2023-02-25 11:25:33 +0530

    Add files via upload

---
+++ Siddharth-s-portfolio---Contest-05-02-23---7d90fq9txhse/src/components/Info.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Info = () => {
+
+
+    return (
+        <div id="info-page">
+
+
+            <h1> Info page </h1>
+            <h2>I am born in India</h2>
+            <h2>and I like to play Cricket </h2>
+            <Link to={"/blog"}><button id="info-to-blog"> Go to Blog</button></Link>
+            <Link to={"/"}><button id="info-to-home"> Go to Home</button></Link>
+        </div>
+    )
+}
+export default Info
