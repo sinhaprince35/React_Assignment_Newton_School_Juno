8e285e0bdaf0aeafb0d763a50bf353631b0539b5
Prince Kumar <112944356+sinhaprince35@users.noreply.github.com>
2023-02-25 11:20:21 +0530

    Add files via upload

---
+++ react-Fragments---Contest-05-02-23---ejpdck1s24p2/src/components/List.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ListItems from './ListItems';
+
+ const List = ({listx}) =>{
+  return(
+    <>
+    
+      <ListItems valuex={listx} />
+    </>
+    
+  )
+}
+
+export default List;
